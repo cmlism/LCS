200d728492c0274cfd73f5820f915611f9fd5e9e
Cemal Üyüklü <cmlism@hotmail.com>
2020-01-09 23:30:56 +0300

    projenin ilk hali

---
+++ MainWin.py
@@ -0,0 +1,71 @@
+import sys
+import subprocess
+import re
+import os
+from optparse import OptionParser
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+from Ui_MainWin import Ui_MainWindow
+
+statusFlag = {'--enable': 'Enabled', '--disable': 'Disabled'}
+
+# Gets the touch device ID
+def getDeviceId():
+    try:
+        data = subprocess.check_output(['xinput', '--list'])
+    except Exception:
+        print("xinput not found!")
+        sys.exit();
+
+    deviceId = 'none'
+
+    for line in data.splitlines():
+        line = line.lower()
+        line = str(line)
+        if "touchpad" in line and "pointer" in line:
+            line = line.strip()
+            match = re.search('id=([0-9]+)', line)
+            deviceId = str(match.group(1))
+            # print(deviceId)
+            # print(line)
+
+    if deviceId == 'none':
+        print('Touch Device not found')
+        sys.exit();
+
+    return deviceId
+
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.main_win)
+        # Connect signals to slot
+        self.ui.checkBox.clicked.connect(lambda: self.checkBoxChangedAction())
+
+
+    def show(self):
+        self.main_win.show()
+
+    def checked(self):
+        self.ui.checkBox.setText('Checked')
+
+    def uncheck(self):
+        self.ui.checkBox.setText('Uncheck')
+
+    def checkBoxChangedAction(self):
+        if (self.ui.checkBox.isChecked() == True):
+            command = "xinput --enable " + getDeviceId()
+            os.system(command)
+        else:
+            command = "xinput --disable " + getDeviceId()
+            os.system(command)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
---
+++ Ui_MainWin.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(764, 587)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 2, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName("checkBox")
+        self.horizontalLayout.addWidget(self.checkBox)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+        self.apply_button = QtWidgets.QPushButton(self.centralwidget)
+        self.apply_button.setObjectName("apply_button")
+        self.gridLayout.addWidget(self.apply_button, 3, 0, 1, 1)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem3, 4, 0, 1, 1)
+        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem4, 0, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.checkBox.setText(_translate("MainWindow", "Touchpad"))
+        self.apply_button.setText(_translate("MainWindow", "Apply"))
